18b496952766ca7e2dc059e502f95ba4045ccf79
Mohamed Aslam M <mohamedaslamcodes@gmail.com>
2025-07-22 16:39:23 +0530

    ...

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,18 +21,15 @@
   h1 {
     font-size: 3rem;
     color: #89b4fa; /* Neon blue */
-    text-shadow: 0 0 10px #89b4fa;
   }
 
   p {
     font-size: 1.25rem;
     color: #f5c2e7; /* Soft purple */
     margin-top: 0.5rem;
-    text-shadow: 0 0 5px #f5c2e7;
   }
 </style>
 
 <div class="container">
   <h1>Coming Soon</h1>
-  <p>Something cool is on the way...</p>
 </div>
